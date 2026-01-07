5cc635c4cc19876c11564cae12496ebf47b3c98c
Deepesh Garg <deepeshgarg6@gmail.com>
2026-01-07 18:04:51 +0530

    feat: add on install hooks

---
+++ ekyc_india/ekyc_india/install.py
@@ -0,0 +1,48 @@
+import frappe
+
+def create_workflow_states():
+	workflow_states = [
+		{
+			"doctype": "Workflow State",
+			"workflow_state_name": "eKYC Requested"
+		},
+		{
+			"doctype": "Workflow State",
+			"workflow_state_name": "eSignature Requested"
+		},
+	]
+
+	for state in workflow_states:
+		if not frappe.db.exists(
+			"Workflow State",
+			{"workflow_state_name": state["workflow_state_name"]},
+		):
+			doc = frappe.get_doc(state)
+			doc.insert(ignore_permissions=True)
+
+def create_workflow_transition_tasks():
+	workflow_transition_tasks = [
+		{
+			"doctype": "Workflow Transition Task",
+			"name": "Send eKYC Request",
+			"tasks":[{
+				"task": "Send eKYC Request",
+				"enabled": 1
+			}]
+		},
+		{
+			"doctype": "Workflow Transition Task",
+			"name": "Make eSignature Request",
+			"tasks":[{
+				"task": "Make eSignature Request",
+				"enabled": 1
+			}]
+		},
+	]
+
+	for transition in workflow_transition_tasks:
+		frappe.get_doc(transition).insert(ignore_permissions=True)
+
+def after_install():
+	create_workflow_states()
+	create_workflow_transition_tasks()
--- ekyc_india/hooks.py
+++ ekyc_india/hooks.py
@@ -86,7 +86,7 @@ app_license = "mit"
 # ------------
 
 # before_install = "ekyc_india.install.before_install"
-# after_install = "ekyc_india.install.after_install"
+after_install = "ekyc_india.install.after_install"
 
 # Uninstallation
 # ------------
@@ -247,7 +247,7 @@ app_license = "mit"
 
 workflow_methods = [
 	{
-		"name": "Make E Signature Request", 
+		"name": "Make eSignature Request", 
 		"method": "ekyc_india.ekyc_india.doctype.digio_settings.digio_settings.make_esignature_request"
 	},
 	{
